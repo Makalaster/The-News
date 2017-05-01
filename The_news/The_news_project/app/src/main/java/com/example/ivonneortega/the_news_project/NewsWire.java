161510a56ade8a2f79124e550e8d40838dc4d118
Ivonne Ortega <ortegaivonne28@gmail.com>
2017-05-01 14:33:15 -0400

    working on api call newsWire

---
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/NewsWire.java
@@ -0,0 +1,12 @@
+package com.example.ivonneortega.the_news_project;
+
+/**
+ * Created by ivonneortega on 5/1/17.
+ */
+
+public class NewsWire {
+
+    public static final String URL = "http://api.walmartlabs.com/v1/search?format=json&apiKey=wecb5fkp8us6yeq64df6snky&query=";
+
+
+}
