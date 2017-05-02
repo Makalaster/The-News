aa473da689af8191e4fd53208b08c54a50eb04f6
Sam Perlow <perlow.sam@gmail.com>
2017-05-02 16:59:11 -0400

    CollectionDemoActivity forgot Log

--- The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/DetailView/CollectionDemoActivity.java
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/DetailView/CollectionDemoActivity.java
@@ -29,6 +29,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
