9c198b40af917fe61d649282755f9fae0eac1cec
Sam Perlow <perlow.sam@gmail.com>
2017-05-04 16:38:47 -0400

    Implemented displaying article in webview

--- The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/webview/WebActivity.java
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/webview/WebActivity.java
@@ -6,19 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.ivonneortega.the_news_project.R;
+import com.example.ivonneortega.the_news_project.detailView.CollectionDemoActivity;
 
 public class WebActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        WebView webView = new WebView(this);
+        setContentView(webView);
 
+        String url = getIntent().getStringExtra(CollectionDemoActivity.URL);
 
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
     }
-
 }
