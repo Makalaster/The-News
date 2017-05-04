ad1d1359f4d9714f03b65811382a6364b45e5989
Sam Perlow <perlow.sam@gmail.com>
2017-05-04 15:04:32 -0400

    Pressing back from main activity now exits.

--- The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/mainActivity/MainActivity.java
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/mainActivity/MainActivity.java
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            moveTaskToBack(true);
         }
     }
 
