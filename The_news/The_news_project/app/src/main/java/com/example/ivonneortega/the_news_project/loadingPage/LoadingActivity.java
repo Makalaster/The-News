f206b0c30435b3d90ad877e208d3cdeb54716384
Sam Perlow <perlow.sam@gmail.com>
2017-05-04 11:12:47 -0400

    Added loading activity

---
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/loadingPage/LoadingActivity.java
@@ -0,0 +1,15 @@
+package com.example.ivonneortega.the_news_project.loadingPage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ivonneortega.the_news_project.R;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
