a7d5ab5fc75012a46e88dfd96cf2cd43ee1874bc
Sam Perlow <perlow.sam@gmail.com>
2017-05-04 16:14:59 -0400

    Added webview activity and set url in detail fragment to launch this activity on click

---
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/webview/WebActivity.java
@@ -0,0 +1,24 @@
+package com.example.ivonneortega.the_news_project.webview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.ivonneortega.the_news_project.R;
+
+public class WebActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
