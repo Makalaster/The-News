cd59d5233cf2d46b1909efd0783d2b2342ce93b8
Sam Perlow <perlow.sam@gmail.com>
2017-05-04 12:56:14 -0400

    App now opens to loading page.

--- The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/loadingPage/LoadingActivity.java
+++ The_news/The_news_project/app/src/main/java/com/example/ivonneortega/the_news_project/loadingPage/LoadingActivity.java
@@ -1,6 +1,7 @@
 package com.example.ivonneortega.the_news_project.loadingPage;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
@@ -18,16 +19,22 @@ import com.example.ivonneortega.the_news_project.R;
 import com.example.ivonneortega.the_news_project.data.Article;
 import com.example.ivonneortega.the_news_project.data.NYTApiData;
 import com.example.ivonneortega.the_news_project.database.DatabaseHelper;
+import com.example.ivonneortega.the_news_project.mainActivity.MainActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+
+import static com.example.ivonneortega.the_news_project.data.NYTApiData.JSON;
+
 public class LoadingActivity extends AppCompatActivity {
-    public static final String JSON = ".json";
     private static final String TAG = "LoadingActivity";
 
     private List<String> mNewsWireList = new ArrayList<>();
@@ -41,38 +48,16 @@ public class LoadingActivity extends AppCompatActivity {
 
         mDb = DatabaseHelper.getInstance(this);
 
-        setupLists();
+        setupTopicLists();
 
-        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-        if (networkInfo != null) {
-            for (String topic : mTopStoriesList) {
-                AsyncTask<String, Void, Void> topTask = new AsyncTask<String, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(String... params) {
-                        queryTopStories(params[0]);
-                        try {
-                            Thread.sleep(500);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                };
-                topTask.execute(topic);
-            }
-
-            for (String topic : mNewsWireList) {
-                queryNewsWire(topic);
-            }
-        }
+        load();
     }
 
-    private void setupLists() {
+    private void setupTopicLists() {
         if (mNewsWireList.isEmpty()) {
             mNewsWireList.add("World");
             mNewsWireList.add("u.s.");
-            mNewsWireList.add("Business Day");
+            mNewsWireList.add("Business+Day");
             mNewsWireList.add("technology");
             mNewsWireList.add("science");
             mNewsWireList.add("Sports");
@@ -96,115 +81,133 @@ public class LoadingActivity extends AppCompatActivity {
         }
     }
 
-    private void queryTopStories(String query) {
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                NYTApiData.URL_TOP_STORY + query + JSON+"?api-key=" + NYTApiData.API_KEY, null,
-                new com.android.volley.Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try
-                        {
-                            JSONArray results = response.getJSONArray("results");
-                            for(int i = 0; i < results.length(); i++)
-                            {
-                                JSONObject article = results.getJSONObject(i);
-                                addArticleToDatabase(article, Article.TRUE);
-                            }
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
+    private void load() {
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null) {
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    for (String topic : mTopStoriesList) {
+                        queryTopStories(topic);
                     }
-                }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onErrorResponse: " + error);
-            }
-        });
 
-        queue.add(jsonObjectRequest);
+                    for (String topic : mNewsWireList) {
+                        queryNewsWire(topic);
+                    }
+
+                    return null;
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+
+                    startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+                }
+            }.execute();
+        }
+    }
+
+    private void queryTopStories(String query) {
+        OkHttpClient topClient = new OkHttpClient();
+
+        String url = NYTApiData.URL_TOP_STORY + query + JSON + "?api-key=" + NYTApiData.API_KEY;
+
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(url)
+                .build();
+
+        Response response;
+        JSONArray articles = null;
+        try {
+            response = topClient.newCall(request).execute();
+            String reply = response.body().string();
+            JSONObject jsonReply = new JSONObject(reply);
+            articles = jsonReply.getJSONArray("results");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        if (articles != null) {
+            for (int i = 0; i < articles.length(); i++) {
+                try {
+                    addArticleToDatabase(articles.getJSONObject(i), Article.TRUE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void queryNewsWire(String query) {
-        RequestQueue queue = Volley.newRequestQueue(this);
+        OkHttpClient wireClient = new OkHttpClient();
 
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                NYTApiData.URL_NEWS_WIRE + query + JSON + "?api-key=" + NYTApiData.API_KEY, null,
-                new com.android.volley.Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try
-                        {
-                            JSONArray results = response.getJSONArray("results");
+        String url = NYTApiData.URL_NEWS_WIRE + query + JSON + "?api-key=" + NYTApiData.API_KEY;
 
-                            for(int i = 0; i < results.length(); i++)
-                            {
-                                JSONObject article = results.getJSONObject(i);
-                                addArticleToDatabase(article, Article.FALSE);
-                            }
+        okhttp3.Request request = new okhttp3.Request.Builder()
+                .url(url)
+                .build();
 
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
+        Response response;
+        JSONArray articles = null;
+        try {
+            response = wireClient.newCall(request).execute();
+            String reply = response.body().string();
+            JSONObject jsonReply = new JSONObject(reply);
+            articles = jsonReply.getJSONArray("results");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-                    }
-                }, new com.android.volley.Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
-                Log.d(TAG, "onErrorResponse: "+error);
+        if (articles != null) {
+            for (int i = 0; i < articles.length(); i++) {
+                try {
+                    addArticleToDatabase(articles.getJSONObject(i), Article.FALSE);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-        });
-        queue.add(jsonObjectRequest);
+        }
     }
 
     public void addArticleToDatabase(JSONObject object, final int fromTopStories) {
-        AsyncTask<JSONObject, Void, Void> dbTask = new AsyncTask<JSONObject, Void, Void>() {
-            @Override
-            protected Void doInBackground(JSONObject... params) {
-                JSONObject object = params[0];
-
-                String url = null;
-                String title = null;
-                String date = null;
-                String category = null;
-                String image = null;
-                boolean hasImage = false;
-                try {
-                    url = object.getString("url");
-                    title = object.getString("title");
-                    date = object.getString("published_date");
-                    category = object.getString("section");
-                    if (!object.getString("multimedia").equals("")) {
-                        JSONArray multimedia = object.getJSONArray("multimedia");
-                        for (int i = 0; i < multimedia.length(); i++) {
-                            JSONObject pic = multimedia.getJSONObject(i);
-                            if (pic.getString("format").equals("Normal") && pic.getString("type").equals("image")) {
-                                image = pic.getString("url");
-                                hasImage = true;
-                            }
-                        }
+        String url = null;
+        String title = null;
+        String date = null;
+        String category = null;
+        String image = null;
+        boolean hasImage = false;
+        try {
+            url = object.getString("url");
+            title = object.getString("title");
+            date = object.getString("published_date");
+            category = object.getString("section");
+            if (!object.getString("multimedia").equals("")) {
+                JSONArray multimedia = object.getJSONArray("multimedia");
+                for (int i = 0; i < multimedia.length(); i++) {
+                    JSONObject pic = multimedia.getJSONObject(i);
+                    if (pic.getString("format").equals("Normal") && pic.getString("type").equals("image")) {
+                        image = pic.getString("url");
+                        hasImage = true;
                     }
-                } catch (JSONException e) {
-                    hasImage = false;
-                    e.printStackTrace();
                 }
-                String source = "New York Times";
-                int isSaved = Article.FALSE;
-
-                if (mDb.getArticleByUrl(url) == null && hasImage) {
-                    mDb.checkSizeAndRemoveOldest();
-                    Log.d(TAG, "doInBackground: " + title);
-                    mDb.insertArticleIntoDatabase(image, title, category, date.substring(0, date.indexOf('T')), null, source, isSaved, fromTopStories, url);
-                }
-
-                return null;
             }
-        };
-        dbTask.execute(object);
+        } catch (JSONException e) {
+            hasImage = false;
+            e.printStackTrace();
+        }
+        String source = "New York Times";
+        int isSaved = Article.FALSE;
+
+        if (mDb.getArticleByUrl(url) == null && hasImage) {
+            mDb.checkSizeAndRemoveOldest();
+            Log.d(TAG, "doInBackground: " + title);
+            mDb.insertArticleIntoDatabase(image, title, category, date.substring(0, date.indexOf('T')), null, source, isSaved, fromTopStories, url);
+        }
     }
 }
