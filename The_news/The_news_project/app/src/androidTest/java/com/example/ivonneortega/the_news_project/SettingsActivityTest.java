3b63442a30ab3e08a028aca10fc55dc2a833e538
Ivonne Ortega <ortegaivonne28@gmail.com>
2017-05-05 14:03:24 -0400

    main activity espresso test done

---
+++ The_news/The_news_project/app/src/androidTest/java/com/example/ivonneortega/the_news_project/SettingsActivityTest.java
@@ -0,0 +1,20 @@
+package com.example.ivonneortega.the_news_project;
+
+import android.support.test.rule.ActivityTestRule;
+
+import com.example.ivonneortega.the_news_project.mainActivity.MainActivity;
+
+import org.junit.Rule;
+
+/**
+ * Created by ivonneortega on 5/5/17.
+ */
+
+public class SettingsActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    
+
+}
